90df25387a4cca731ac1c0a9f6db0678b463b944
nenquen <nenquen@codeberg.org>
2026-07-26 22:53:59 +0300

    rename: dms tab to whispers

--- api/index.js
+++ api/index.js
@@ -608,7 +608,7 @@ th{color:#666;font-size:10px;text-transform:uppercase;font-weight:400}
 <button class="active" onclick="switchTab('dashboard')"><span class=l>[</span><span class=m>&nbsp;dashboard&nbsp;</span><span class=r>]</span></button>
 <button onclick="switchTab('messages')"><span class=l>[</span><span class=m>&nbsp;messages&nbsp;&nbsp;</span><span class=r>]</span></button>
 <button onclick="switchTab('members')"><span class=l>[</span><span class=m>&nbsp;members&nbsp;&nbsp;&nbsp;</span><span class=r>]</span></button>
-<button onclick="switchTab('dms')"><span class=l>[</span><span class=m>&nbsp;dms&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span class=r>]</span></button>
+<button onclick="switchTab('dms')"><span class=l>[</span><span class=m>&nbsp;whispers&nbsp;&nbsp;</span><span class=r>]</span></button>
 <button id="logoutBtn" onclick="logout()">logout</button>
 </div>
 <div class="main">
