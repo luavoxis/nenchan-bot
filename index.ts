af17a1394a99d17e504db6e83517cdea8d5e1328
nenquen <nenquen@codeberg.org>
2026-07-27 03:22:41 +0300

    banned tab moved under members, sidebar order fixed

--- index.ts
+++ index.ts
@@ -286,9 +286,9 @@ th{color:#666;font-size:10px;text-transform:uppercase;font-weight:600}
 <h1>nenchan v1.0</h1>
 <button class="active" onclick="switchTab('dashboard')"><span class=l>[</span><span class=m>&nbsp;dashboard&nbsp;</span><span class=r>]</span></button>
 <button onclick="switchTab('members')"><span class=l>[</span><span class=m>&nbsp;members&nbsp;&nbsp;&nbsp;</span><span class=r>]</span></button>
+<button onclick="switchTab('bans')"><span class=l>[</span><span class=m>&nbsp;banned&nbsp;&nbsp;&nbsp;</span><span class=r>]</span></button>
 <button onclick="switchTab('messages')"><span class=l>[</span><span class=m>&nbsp;messages&nbsp;&nbsp;</span><span class=r>]</span></button>
 <button onclick="switchTab('dms')"><span class=l>[</span><span class=m>&nbsp;whispers&nbsp;&nbsp;</span><span class=r>]</span></button>
-<button onclick="switchTab('bans')"><span class=l>[</span><span class=m>&nbsp;banned&nbsp;&nbsp;&nbsp;</span><span class=r>]</span></button>
 <button id="logoutBtn" onclick="logout()">logout</button>
 </div>
 <div class="main">
